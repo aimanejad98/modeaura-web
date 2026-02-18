3cff1d6792119752d772d78f10eb9a388bf040c0
Ayman <modeaura1@gmail.com>
2026-02-18 13:52:03 -0500

    Fix domain extension in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import prisma from '@/lib/db';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const baseUrl = 'https://www.modeaura.com';
+    const baseUrl = 'https://www.modeaura.ca';
 
     // Static pages
     const staticPages = [
